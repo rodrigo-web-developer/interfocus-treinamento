cb5d177066965ba71aec7dea88af68eff762ee60
unknown <x10rodrigo@gmail.com>
2024-05-11 13:07:58 -0300

    proxy validator removed

--- InterfocusConsole/Entidades/Curso.cs
+++ InterfocusConsole/Entidades/Curso.cs
@@ -8,12 +8,12 @@ namespace InterfocusConsole.Entidades
 {
     public class Curso
     {
-        public virtual int Id { get; set; }
-        public virtual string Nome { get; set; }
-        public virtual string Descricao { get; set; }
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
         // 0 - iniciante, 1 - intermediario, 2 - avançado, 3 - expert
-        public virtual NivelCurso Nivel { get; set; }
-        public virtual int Duracao { get; set; }
+        public NivelCurso Nivel { get; set; }
+        public int Duracao { get; set; }
 
         void Metodo()
         {
